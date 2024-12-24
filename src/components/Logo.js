2c6f7ed2c443e5887eb054f713e14bb29b60e37c
Mehdi <mehdi_sellam@hotmail.com>
2024-12-24 12:31:43 +0100

    Corrected

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,16 +5,20 @@ import { motion } from "framer-motion";
 const MotionLink = motion(Link);
 
 function Logo() {
+  const [hovered, setHovered] = useState(false);
   return (
     <div className="flex items-center justify-center mt-2">
       <MotionLink
         href="/"
+        onHoverStart={() => setHovered(true)}
+      onHoverEnd={() => setHovered(false)}
         className="w-16 h-16 bg-dark text-light flex flex-col items-center justify-center rounded-full text-xl font-bold "
         whileHover={{
           
           width: '150px',
-          transition:{duration:1}
+          
         }}
+        transition={{ type: 'spring', stiffness: 300, damping: 20 }}
         
       >
         <motion.div className="p-0">
